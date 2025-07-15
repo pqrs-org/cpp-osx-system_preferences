ba85ccc74461b3b3f6d4b4bd1cdc16fd6f2d6cfc
Takayama Fumihiko <tekezo@pqrs.org>
2025-07-15 21:48:40 +0900

    http -> https

--- include/pqrs/osx/system_preferences.hpp
+++ include/pqrs/osx/system_preferences.hpp
@@ -4,7 +4,7 @@
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "system_preferences/keyboard_type_key.hpp"
 #include "system_preferences/properties.hpp"
